c2e5c1eb83e2fc1f6c4cd688fa7ae96eecfa2bd6
AltovoIta <cavila@fi.uba.ar>
2024-10-30 22:33:28 -0300

    clear client data for counter_by_platform

--- counter_by_platform/counter_by_platform.py
+++ counter_by_platform/counter_by_platform.py
@@ -47,7 +47,6 @@ class CounterByPlatform:
 
     def __handle_message(self, delivery_tag: int, body: List[List[str]]):
         body = self._middleware.get_rows_from_message(body)
-        #for message in body:
 
         logging.debug(f"GOT BATCH: {body}")
 
@@ -61,6 +60,16 @@ class CounterByPlatform:
             return
 
 
+        count_per_record_by_client_id = self.__count_per_client_id(body)
+
+        storage.sum_platform_batch_to_records_per_client(
+            self._config["STORAGE_DIR"],
+            count_per_record_by_client_id,
+        )
+
+        self._middleware.ack(delivery_tag)
+
+    def __count_per_client_id(self, body: list[list]):
         count_per_record_by_client_id = {}
         for record in body:
             client_id = record[REGULAR_MESSAGE_SESSION_ID]
@@ -72,22 +81,10 @@ class CounterByPlatform:
             count_per_record_by_client_id[client_id][record_id] = (
                 count_per_record_by_client_id[client_id].get(record_id, 0) + 1
             )
-
-        storage.sum_platform_batch_to_records_per_client(
-            self._config["STORAGE_DIR"],
-            count_per_record_by_client_id,
-        )
-
-        # logging.debug(message)
-        # session_id = message[REGULAR_MESSAGE_SESSION_ID]
-        # field_to_count = message[REGULAR_MESSAGE_FIELD_TO_COUNT_BY]
-
-        # self.__count(field_to_count, session_id)
-
-        self._middleware.ack(delivery_tag)
+            
+        return count_per_record_by_client_id
 
     def __send_results(self, session_id: str):
-        # TODO: READ FROM STORAGE
         publish_queue = self._config["PUBLISH_QUEUE"]
         self._middleware.create_queue(publish_queue)
 
@@ -97,7 +94,7 @@ class CounterByPlatform:
         for record in reader:
             logging.debug(f"sending record: {record}")
             if self._got_sigterm:
-                #should send everithing so i can ack before closing 
+                #should send everything so i can ack before closing 
                 # or return false so end is not acked and i dont send the results?
                 return
             
@@ -111,31 +108,7 @@ class CounterByPlatform:
         )
         logging.debug(f"Sent results to queue {publish_queue}")
 
-
-        # for platform, count in self._count_dict[session_id].items():
-        #     logging.debug(f"{platform}, {count}")
-        #     if self._got_sigterm:
-        #         return
-
-        #     # encoded_msg = self.protocol.encode([key, str(value)])
-        #     self._middleware.publish(
-        #         [session_id, platform, str(count)], queue_name=publish_queue
-        #     )
-
-        # encoded_msg = self.protocol.encode([END_TRANSMISSION_MESSAGE])
-        # self.middleware.publish(encoded_msg, queue_name=self.config["PUBLISH_QUEUE"])
-
-    # def __count(self, field_to_count: str, session_id: str):
-    #     # TODO: SAVE AND UPDATE ON STORAGE
-
-    #     # session_id = {windows: algo, mac: algo, linux: algo}
-
-    #     if not session_id in self._count_dict:
-    #         self._count_dict[session_id] = {}
-
-    #     session_id_field_count = self._count_dict[session_id].get(field_to_count, 0)
-
-    #     self._count_dict[session_id][field_to_count] = session_id_field_count + 1
+        storage.delete_directory(client_dir)
 
     def __sigterm_handler(self, signal, frame):
         logging.debug("Got SIGTERM")
